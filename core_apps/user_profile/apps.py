03d211a6c94078689224392ad17edd835eddd966
otavius <otavius1994@otavius.localdomain>
2026-09-09 18:13:18 -0400

    chore: create and configure local apps

---
+++ core_apps/user_profile/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _ 
+
+class UserProfileConfig(AppConfig):
+    name = "core_apps.user_profile"
+    verbose_name = _("User Profile")
