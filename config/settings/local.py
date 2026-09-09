2f6c6833dd582f70d97d5e10957081f78600de7f
otavius <otavius1994@otavius.localdomain>
2026-09-09 18:04:47 -0400

    chore: add env variables

--- config/settings/local.py
+++ config/settings/local.py
@@ -27,6 +27,6 @@ EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = getenv("EMAIL_HOST")
 EMAIL_PORT = getenv("EMAIL_PORT")
 DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
-DOMAIN = getenv(:DOMAIN)
+DOMAIN = getenv("DOMAIN")
 
 MAX_UPLOAD_SIZE = 1 * 1024 * 1024
